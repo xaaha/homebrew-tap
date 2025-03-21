2b9808f074b0b1bba15db82f93fdf4cf77d56535
xaaha <pratik.philos@yahoo.com>
2025-03-21 17:11:34 -0400

    brew formula

---
+++ Formula/hulak.rb
@@ -0,0 +1,23 @@
+class Hulak < Formula
+  desc 'File Based API client for terminal nerds'
+  homepage 'https://github.com/xaaha/hulak'
+  url 'https://github.com/xaaha/hulak/archive/refs/tags/v0.1.0-beta.1.tar.gz'
+  sha256 '191a999ddbdf20b4e7219c23c90ad12e599e51a885da0acc966d1993118ffb14'
+  # Beta release
+  version '0.1.0-beta.1'
+  license 'MIT'
+  head 'https://github.com/xaaha/hulak.git', branch: 'main'
+
+  depends_on 'go' => :build
+
+  def install
+    system 'go', 'build', *std_go_args(ldflags: '-s -w')
+    man1.install 'man/hulak.1'
+  end
+
+  test do
+    assert_match 'Api Usage:', shell_output("#{bin}/hulak help")
+    system bin / 'hulak', 'init'
+    assert_predicate testpath / 'env' / 'global.env', :exist?
+  end
+end
