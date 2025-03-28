55c46c2885d121d1e77600c51d723099ba7100f0
xaaha <pratik.philos@yahoo.com>
2025-03-28 09:00:08 -0400

    hulak beta 2 release

--- Formula/hulak.rb
+++ Formula/hulak.rb
@@ -1,10 +1,10 @@
 class Hulak < Formula
   desc 'File Based API client for terminal nerds'
   homepage 'https://github.com/xaaha/hulak'
-  url 'https://github.com/xaaha/hulak/archive/refs/tags/v0.1.0-beta.1.tar.gz'
-  sha256 '191a999ddbdf20b4e7219c23c90ad12e599e51a885da0acc966d1993118ffb14'
+  url 'https://github.com/xaaha/hulak/archive/refs/tags/v0.1.0-beta.2.tar.gz'
+  sha256 'f25fc34e108fe25456e82444212edc0acf684b0fe6262a965ff9ac008ec6184b'
   # Beta release
-  version '0.1.0-beta.1'
+  version '0.1.0-beta.2'
   license 'MIT'
   head 'https://github.com/xaaha/hulak.git', branch: 'main'
 
