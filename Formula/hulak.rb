fd2eef38d33e99528c3e691ce2ae947789efe238
xaaha <pratik.philos@yahoo.com>
2025-03-28 11:24:56 -0400

    bug fix beta 3

--- Formula/hulak.rb
+++ Formula/hulak.rb
@@ -1,10 +1,10 @@
 class Hulak < Formula
   desc 'File Based API client for terminal nerds'
   homepage 'https://github.com/xaaha/hulak'
-  url 'https://github.com/xaaha/hulak/archive/refs/tags/v0.1.0-beta.2.tar.gz'
-  sha256 'f25fc34e108fe25456e82444212edc0acf684b0fe6262a965ff9ac008ec6184b'
+  url 'https://github.com/xaaha/hulak/archive/refs/tags/v0.1.0-beta.3.tar.gz'
+  sha256 '5252c9451a63cb4c681c794d11f182036f4e19e3b2797d8de395de83f76e5a8f'
   # Beta release
-  version '0.1.0-beta.2'
+  version '0.1.0-beta.3'
   license 'MIT'
   head 'https://github.com/xaaha/hulak.git', branch: 'main'
 
